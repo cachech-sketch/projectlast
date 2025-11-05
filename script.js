10ab2e1f3906c428350b7ec1d6edac9fc5ca4bec
cachech-sketch <cachech@gmail.com>
2025-11-05 11:32:18 -0600

    protype

---
+++ script.js
@@ -0,0 +1,21 @@
+//board unfinshed workin progress 
+////
+
+
+const colorfortiles=["orange","blue", "black","pale green"];
+const totalSpacesoftiles=67
+function candyLandboard(){
+
+}
+//if palyer hit a squre with  this on they will  go back a space 
+function Spacebackspace(){}
+function moveplayer(){
+
+}
+function Cardsforpalyer(){}
+// a player wins???
+ function winnerwinner(){}
+//https://i.etsystatic.com/6017061/r/il/74a33b/2228647266/il_570xN.2228647266_h4yy.jpg
+//https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.etsy.com%2Flisting%2F596432855%2Fvintage-original-candy-land-milton&psig=AOvVaw1iBcZDRItiU2vLGo8OdG4U&ust=1762446899601000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKCGhr2425ADFQAAAAAdAAAAABAL
+
+//https://codepen.io/cjgammon/pen/RWpYVj?utm
